846d9df21d7b004a00c296fd8d2b7a66d2b768ad
Vedashree Bane <vedashreebane@gmail.com>
2026-04-03 16:12:57 -0400

    Fix import paths in test files

--- Lab6_Github_Actions/test/test_pytest.py
+++ Lab6_Github_Actions/test/test_pytest.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
-from src.calculator import add, subtract, multiply, combined, divide
+from calculator import add, subtract, multiply, combined, divide
 
 
 # Addition tests
